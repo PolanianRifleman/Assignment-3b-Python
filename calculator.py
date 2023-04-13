e48eaef0f6d58f7c43f716dcd4f0eeb8995c20e0
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-04-13 14:01:09 +0000

    finished

--- calculator.py
+++ calculator.py
@@ -1,28 +1,41 @@
-#st = float(0)
+size = float(0)
 
 while True:
 
-    s = input("Would you like Large or Extra Large?").lower()
-    while s != "large" and s != "extra large":
+    s = input("Would you like \"l\" or \"xl\" pizza? ").lower()
+    while s != "l" and s != "xl":
         print("\nINVALID INPUT")
-        s = input("Would you like Large or Extra Large?").lower()
+        s = input("Would you like \"l\" or \"xl\" pizza? ").lower()
 
-    t = (input("How many toppings? "))
-    t = float(t)
+    # initialize t out of range just to enter while loop
+    t = float(10)
     while t not in range (0,5):
-        print("INVALID INPUT")
-        t = (input("How many toppings? "))
-        t = float()
+        #print("INVALID INPUT")
+        t = input("How many toppings? (0-4) ")
+        # check for input that can't be converted to float
+        try:
+            t = float(t)
+        except ValueError:
+            continue
 
-    if s.casefold() == "large":
-        st = 6
-    elif s.casefold() == "extra large":
-        st = 10
-    if t != 0:
-        st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*t + 0.15), 2))
-    tax = round(st*0.13, 2)
+    if s.casefold() == "l":
+        size = 6
+    elif s.casefold() == "xl":
+        size = 10
+    #if t != 0:
+        #size = (round(size+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*t + 0.15), 2))
+    #    size = (round((0.0167*(t**3) - 0.1*(t**2) + 0.9333*t + 0.15), 2))
+    match t:
+        case 1: topping = 1.00
+        case 2: topping = 1.75
+        case 3: topping = 2.50
+        case 4: topping = 3.35
+        case _: topping = 0.0
+    # update price with toppings
+    size = (round(size+topping, 2))
+    tax = round(size*0.13, 2)
 
-    print("Subtotal      $" + str(st))
-    print("Tax      $" + str(tax))
-    print("Total     $" + str(round(float(st + tax), 2)))
+    print("Subtotal  $" + str(size))
+    print("Tax       $" + str(tax))
+    print("Total     $" + str(round(float(size + tax), 2)))
     break
